ef63c2abefd94123e84e2d97f88ff2549261eeb1
halhyane <houssamalhyan5@gmail.com>
2025-12-28 15:27:33 +0100

    add history

---
+++ handlers/senhistory.go
@@ -0,0 +1,10 @@
+package zone
+import "net"
+// sending history for new joigned clients
+func sendHistory(conn net.Conn) {
+	logMu.Lock()
+	defer logMu.Unlock()
+	for _, msg := range messageLog {
+		conn.Write([]byte(msg + "\n"))
+	}
+}
