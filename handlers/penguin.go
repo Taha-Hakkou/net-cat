4e76337f8981684dd71694468056e85038490ad9
yzarhoun <youneszarhouni714@gmail.com>
2025-12-28 15:46:20 +0100

    add pen

---
+++ handlers/penguin.go
@@ -0,0 +1,11 @@
+package zone
+
+import "os"
+//a function to read the penguin file
+func peng()[]byte{
+hellomsg ,err:= os.ReadFile("handlers/peng.txt")
+if err != nil{
+	return []byte("erorr read peng")
+}
+return hellomsg
+}
